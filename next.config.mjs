62213d7dba5917c4e8104ecba469d2d509589f4a
Shahmeer Ali <problemssolve196@gmail.com>
2024-10-03 13:41:26 +0500

    change

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 import { withNextVideo } from "next-video/process";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output:"export"
+    
 };
 
 export default withNextVideo(nextConfig, { folder: 'public/images/hero.mp4' });
